b214a1e1e5f11fba0d0d2632412e82563001c6a5
Zane Ice <zaneice10@gmail.com>
2019-10-29 11:53:17 -0400

    WORKING CODE FROM MATT

--- Thrive/ContentView.swift
+++ Thrive/ContentView.swift
@@ -1,61 +1,84 @@
 //
+
 //  ContentView.swift
+
 //  Thrive
+
 //
+
 //  Created by Zane Ice on 10/22/19.
+
 //  Copyright © 2019 Thrive. All rights reserved.
+
 //
+
 
 import SwiftUI
 
+
 struct ContentView: View {
+
     @State private var selection = 0
+
  
+
     var body: some View {
+
     
+
         TabView(selection: $selection){
-			VStack {
-				Text("Thrive")
-					.font(.title)
-					.fontWeight(.bold)
-					.tabItem {
-						VStack {
-							Image("home")
-							Text("Home")
-						}
-					}
-				.tag(0)
-				
-				HStack {
-					Text("Meet the Health Center")
-					Text("Identity")
-				}
-				HStack {
-					Text("Wellness")
-					Text("Sexual Harrassment")
-				}
-				HStack {
-					Text("Time Management")
-					Text("Mental Disorders")
-				}
-			
-			}
-			
-            Text("Contact")
+
+            Text("Thrive")
+
                 .font(.title)
-                .tabItem {
+
+.fontWeight(.black)
+
+.tabItem {
+
                     VStack {
-                        Image("contact")
-                        Text("Contact")
+
+                        Image("home")
+
+                        Text("Home")
+
                     }
+
                 }
+
+                .tag(0)
+
+            Text("Second View")
+
+                .font(.title)
+
+                .tabItem {
+
+                    VStack {
+
+                        Image("contact")
+
+                        Text("Contact")
+
+                    }
+
+                }
+
                 .tag(1)
-        }
+
+        }//
+
     }
+
 }
 
+
 struct ContentView_Previews: PreviewProvider {
+
     static var previews: some View {
+
         ContentView()
+
     }
+
 }
