25f3e9d04b7a2cf75471707f4641ac2f1781f239
Zane Ice <zaneice10@gmail.com>
2019-11-02 14:36:42 -0400

    added mental health file

---
+++ Thrive/Mental Health.swift
@@ -0,0 +1,21 @@
+//
+//  Mental Health.swift
+//  Thrive
+//
+//  Created by Zane Ice on 11/2/19.
+//  Copyright © 2019 CSC590. All rights reserved.
+//
+
+import SwiftUI
+
+struct Mental_Health: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Mental_Health_Previews: PreviewProvider {
+    static var previews: some View {
+        Mental_Health()
+    }
+}
