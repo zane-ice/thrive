d2224c469ac6df7407941c4390f9a507c9cdef50
Zane Ice <zaneice10@gmail.com>
2019-11-18 12:34:34 -0500

    liontips

--- Thrive/lionTip.swift
+++ Thrive/lionTip.swift
@@ -9,13 +9,19 @@
 import SwiftUI
 
 struct lionTip: View {
-    let tips = ["ex1", "ex2", "ex3"]
+    let tips = ["If you have three major assignments on one day, you can talk to your teachers and get one of them moved.", "The Health Center provides free condoms at the front desk.", "Stick to a consistent bedtime. It’s biologically impossible to “make up sleep.","If you leave while obtaining illegal visitations because you feel unsafe, you will not face disciplinary action.","The Planned Parenthood is within walking distance of Exeter! You can get condoms, birth control, and much more.","Buddhist Meditation meets every Friday evening in the church."]
     var body: some View {
-        ZStack{
-        Text("Lion Tip: " + tips.randomElement()!)
-            .font(.subheadline)
-            .fontWeight(.medium)
+        VStack{
+        Text("Lion Tip")
+            .foregroundColor(Color.white)
+            Text(tips.randomElement()!)
+                .font(.callout)
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.white)
+            .padding(10)
         }
+        
     }
 }
 
